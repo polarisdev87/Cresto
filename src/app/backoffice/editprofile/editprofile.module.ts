c0242ee51fa9f6f528cafcd9c0cdaf87cd9f53bd
polarisdev87 <polarisdev87@gmail.com>
2018-06-04 16:16:12 +0300

    fix

--- src/app/backoffice/editprofile/editprofile.module.ts
+++ src/app/backoffice/editprofile/editprofile.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EditProfileComponent} from './editprofile.component';
 import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
       {path: '', component: EditProfileComponent}
-    ])
+    ]),
+    FormsModule
   ],
   declarations: [EditProfileComponent]
 })
