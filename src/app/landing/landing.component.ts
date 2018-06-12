6609f9d76c8f0b2d519af6d1707a3f519d37902e
polarisdev87 <polarisdev87@gmail.com>
2018-06-12 18:57:05 +0300

    fix styles landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.bundle.css', './landing.component.css']
 })
 export class LandingComponent {
+  ngOnInit() {
+    const el1 = document.createElement('script');
+    const el2 = document.createElement('script');
+    el1.src = 'assets/js/jquery.bundle.js';
+    el1.onload = () => {
+      el2.src = 'assets/js/script.js';
+      document.body.appendChild(el2);
+    }
+    document.body.appendChild(el1);
+
+  }
 }
