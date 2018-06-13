c140e3c6755e4dbaecb63ac2cca6a48e4c4bc2a2
polarisdev87 <polarisdev87@gmail.com>
2018-06-13 16:04:02 +0300

    fix async pipe

--- src/app/backoffice/myreferrals/myreferrals.component.ts
+++ src/app/backoffice/myreferrals/myreferrals.component.ts
@@ -1,26 +1,31 @@
 import { environment } from '../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
-import { Store } from "@ngrx/store";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-myreferrals',
   templateUrl: './myreferrals.component.html',
   styleUrls: ['./myreferrals.component.css']
 })
-export class MyreferralsComponent implements OnInit {
-  isLoaded = false;
-  username;
-  referrals = [];
-  referalLink;
-
-  constructor(
+export class MyreferralsComponent implements OnInit, OnDestroy {
+  public isLoaded = false;
+  public username;
+  public referrals = [];
+  public referralLink;
+  public userSubscription: Subscription;
+  public constructor(
     private _store: Store<StoreStates>
   ) {
   }
 
-  ngOnInit() {
-    this._store.select('auth', 'user').subscribe((user: User) => {
-      this.referalLink = `${environment.domain}/${user.referralHash}`;
+  public ngOnInit(): void {
+    this.userSubscription = this._store.select('auth', 'user').subscribe((user: User) => {
+      this.referralLink = `${environment.domain}/${user.referralHash}`;
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 }
