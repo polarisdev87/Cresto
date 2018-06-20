bbb85fb7c67f6d27d75fdf1299b64240618e60aa
polarisdev87 <polarisdev87@gmail.com>
2018-06-20 13:34:31 +0300

    Team (#44)
    * fix styles
    
    * fix styles
    
    * fix team

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -9,6 +9,7 @@ import { WhatMakesComponent } from './what-makes/what-makes.component';
 import { TokenAllocationComponent } from './token-allocation/token-allocation.component';
 import { RoadmapComponent } from './roadmap/roadmap.component';
 import { TokenSaleComponent } from './token-sale/token-sale.component';
+import { TeamComponent } from './team/team.component';
 import { ParticipateComponent } from './participate/participate.component';
 import { MembersComponent } from './members/members.component';
 
@@ -21,7 +22,7 @@ import { MembersComponent } from './members/members.component';
       }
     ])
   ],
-  declarations: [LandingComponent, HeaderComponent, WhatTokenComponent, WhyTokenComponent, WhatMakesComponent, TokenAllocationComponent, RoadmapComponent, TokenSaleComponent, ParticipateComponent, MembersComponent],
+  declarations: [LandingComponent, HeaderComponent, WhatTokenComponent, WhyTokenComponent, WhatMakesComponent, TokenAllocationComponent, RoadmapComponent, TokenSaleComponent, ParticipateComponent, MembersComponent, TeamComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class LandingModule {
