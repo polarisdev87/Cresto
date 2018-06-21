0b79339fa0d1c1254d25f04d954def8a799e2f2b
polarisdev87 <polarisdev87@gmail.com>
2018-06-21 16:56:28 +0300

    fix styles nav

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -248,4 +248,55 @@ $(document).ready(function() {
   particlesJS("particles-token", particlesOptions);
   particlesJS("particles-team", particlesOptionsTeam);
 
+  /// Cache selectors
+  let lastId,
+    topMenu = $(".main-nav"),
+    navMobile = $('.navigation-wrapper'),
+    mobileNav = $('.hamburger-icon'),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find(".scroll"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      let item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+  // Bind click handler to menu items
+  // so we can get a fancy scroll animation
+  menuItems.click(function(e){
+    let href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+    navMobile.removeClass('open');
+    mobileNav.removeClass('active');
+    $('html, body').stop().animate({
+      scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+  });
+
+  // Bind to scroll
+  $(window).scroll(function(){
+    // Get container scroll position
+    let fromTop = $(this).scrollTop()+topMenuHeight;
+
+    // Get id of current scroll item
+    let cur = scrollItems.map(function(){
+      if ($(this).offset().top < fromTop)
+        return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    let id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+      lastId = id;
+      // Set/remove active class
+      menuItems
+        .parent().removeClass("active")
+        .end().filter("[href='#"+id+"']").parent().addClass("active");
+    }
+  });
+
+
 });
