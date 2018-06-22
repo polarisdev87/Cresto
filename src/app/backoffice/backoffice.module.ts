28256de1b65e82a9fae13b94f14f41cc4aab383e
polarisdev87 <polarisdev87@gmail.com>
2018-06-22 15:30:19 +0300

    create sidebar

--- src/app/backoffice/backoffice.module.ts
+++ src/app/backoffice/backoffice.module.ts
@@ -1,4 +1,4 @@
-import { UiModule } from './../shared/module/ui/ui.module';
+import { UiModule } from '../shared/module/ui/ui.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
