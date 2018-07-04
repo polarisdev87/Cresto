a5eeacdaef66242fdf857a16f4a503a0f48a4358
polarisdev87 <polarisdev87@gmail.com>
2018-07-04 20:17:41 +0300

    create markup settings page

--- src/app/backoffice/settings/personal-information/personal-information.component.ts
+++ src/app/backoffice/settings/personal-information/personal-information.component.ts
@@ -9,24 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class PersonalInformationComponent implements OnInit {
 
   public switch: boolean = true;
-  // form : FormGroup;
 
   constructor() { 
-    // this.form = formBuilder.group({
-    //   "userName": ["Robert Williams", Validators.required],
-    //   "userEmail": ["robert@gmail.com", [
-    //     Validators.required,
-    //     Validators.pattern("[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}")
-    //   ]],
-    //   "userSponsor": ["crestreferral001"]
-    // });
+
   }
 
   ngOnInit() {
   }
 
   setDisabledState() {
-    this.switch = !this.switch
+    this.switch = !this.switch;
   }
 
 }
