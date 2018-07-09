a9e0c4e823961e6e09e94028d6a9bc35aad6fe0d
polarisdev87 <polarisdev87@gmail.com>
2018-07-09 19:14:49 +0300

    new styles and logic for components

--- src/app/content/backoffice/routes.ts
+++ src/app/content/backoffice/routes.ts
@@ -42,7 +42,7 @@ export const routes = [
         loadChildren: './content/comingsoon/comingsoon.module#ComingsoonModule',
         data: {
           title: 'STAKING',
-          subtitle: `Our staking platform allows you to earn monthly dividends in the form of CSTT tokens (up to 10%), when you tokens for 30 days or more.`,
+          subtitle: `Our staking platform allows you to earn monthly dividends in the form of CSTT tokens (up to 10%), when you stake tokens for 30 days or more.`,
           backgroundImg: '../../../assets/images/staking.png'
         },
         canLoad: [AccessGuardService]
