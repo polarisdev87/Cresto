a3163f94f7fadc5a4918c1630e04938caf3e9d2a
polarisdev87 <polarisdev87@gmail.com>
2018-09-16 15:43:36 -0400

    [Update] Update contest page

--- src/assets/js/contest.js
+++ src/assets/js/contest.js
@@ -111,7 +111,7 @@ $(document).ready(function(){
   };
   particlesJS("particles", particlesOptions);
   $("#countdown").countdown({
-    date: "12 September 2018 08:00:00",
+    date: "24 September 2018 00:00:00",
     format: "on"
   },
   
