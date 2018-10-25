3d2b5af23c0bc79e1c99b30cf0dd66ad81665fc9
polarisdev87 <polarisdev87@gmail.com>
2018-10-25 13:39:27 -0400

    [Update] Update dashboard notice texts

--- src/app/content/backoffice/content/dashboard/instruction/instruction.component.ts
+++ src/app/content/backoffice/content/dashboard/instruction/instruction.component.ts
@@ -18,16 +18,11 @@ export class InstructionComponent {
     },
     {
       label: 'Pre-Sale ICO',
-      text: 'Price is $0.25 per token, with min/max token purchase of 100/10,000 CSTT. This will allow our early ' +
-      'supporters to partake at the lowest price possible. 100% of raised fund will go towards promoting the ICO ' +
-      'and kick-off full platform development.'
+      text: 'Price is $0.25 per token. This will allow our early supporters to partake at the lowest price possible. 100% of raised fund will go towards promoting the ICO and kick-off full platform development.'
     },
     {
       label: 'Main Sale ICO',
-      text: 'Tokens will be sold in 16 rounds of 400,000 tokens per round. Price will start at $0.35 and increase by ' +
-      '$0.10 per round with the last round price of $1.85. We anticipate our platform to grow to over 250,000 ' +
-      'members within the first 90 days after ICO with a potential CSTT value beyond $15. Don’t MISS the Opportunity ' +
-      'to get CSTT first!'
+      text: 'Tokens will be sold in 8 rounds of 800,000 tokens per round. Price will start at $0.30 and increase by $0.20 per round with the last round price of $1.70. We anticipate our platform to grow to over 250,000 members within the first 90 days. Don’t MISS the Opportunity to get CSTT first!'
     }
   ];
 }
